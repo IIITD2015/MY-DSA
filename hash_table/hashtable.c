30e83bcede8c336f179a7bf551839eef78c1b61b
PARITOSH HIRA <paritoshheeraiiitb@gmail.com>
2015-08-05 11:35:59 -0700

    creat node and insertion at d end part is done
    working on insertion but dont knw how to access the using pointer of
    hashTable? ..so only error is der plz check it

--- hash_table/hashtable.c
+++ hash_table/hashtable.c
@@ -1,7 +1,6 @@
-
-#include stdio.h
-#include stdlib.h
-#include string.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct node{
 	int key,rollno;
@@ -13,47 +12,70 @@ struct hashTable{
 	struct node*head;
 	
 	};
-struct hasTable*hash;
+struct hasTable* hash[];
 
-int eleCount;
-struct node* createNode(int rollno,int marks){
+int eleCount,hashIndex;
+struct node* createNode(int key,int rollno,int marks){
+		         
+				hashIndex=key%eleCount; 
+	    		struct hashTable* newHash=(struct hashTable*)malloc(eleCount*sizeof(struct hashTable));
+      			struct node* newnode=(struct node*)malloc(sizeof(struct node));
+        		newnode->rollno=rollno;
+        		newnode->marks=marks;
+        		newnode->next=NULL;
+        		return newnode;
+    
+}
+	 
 
-void insertFucn(int key,int rollno,int marks,eleCount){
-	int hashIndex=key%eleCount; 
-	struct hashTable*newHash=(struct hashTable*)malloc(eleCount*sizeof(struct hashTable));
-        if(!hashTable[hashIndex]){
-		struct node*newnode=(struct node*)malloc(sizeof(sturct node));
-        	hashTable[hashIndex].head=newnode;
-        	newnode->rollno=rollno;
-        	newnode->marks=marks;
-	}
-	else{
-		hashTable[hashIndex].head=newnode;
+void insertFucn(int key,int rollno,int marks,int eleCount){
+				struct node* newnode;
+		
+		       if(!hash[hashIndex]){
+		       	    
+		       		newnode=createNode(key,rollno,marks);
+		       		hash[hashIndex].head=newnode;
+		       		printf("hash table does nt exist");
+		       }
+
+	else{ struct node *temp;     //insertion at d end
+			temp=hash[hashIndex].head;
+		    while(temp!=NULL){
+		    temp=temp->next;
+		}
+			
+			temp->next=newnode;
+			newnode->next=NULL;
+	
 	}
 }
 
-void insertFucn(int key,int rollno,int marks){
 	
 	
 
 int main(){
-       int roll_no,marks,eleCount,key;
-	char option[10];
+       int roll_no,marks,key;
+	int option;
 	scanf("%d",eleCount);
         printf("Select any one of the following options\n");
 	printf("*******************************\n");
 	printf("1.write insert for insertion");
 	printf("2.write delete for deletion");
 	printf("3.write search for searching");
-        scanf("%s",option);
+        scanf("%d",option);
 	switch(option)
 		case 1:
-               	if(strcmp(option,insert)==0){
-		scanf("%d",key);
-                scanf("%d",roll_no);
-                scanf("%d",marks);
-                insertFucn(key,roll_no,marks,eleCount);
-	}
-	
+			{
+			
+               	if(option=1){
+					scanf("%d",key);
+               		scanf("%d",roll_no);
+                	scanf("%d",marks);
+                	insertFucn(key,roll_no,marks,eleCount);
+                	
+			}
+	        break;
 
-
+}
+ 			return 0;
+ 		}
