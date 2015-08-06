6c5f717b33326832226ea4628cd30f6673a6c6a7
paritosh hira <paritoshheeraiiitb@gmail.com>
2015-08-06 22:14:07 +0530

    half way

--- hash_table/hash-pari.c
+++ hash_table/hash-pari.c
@@ -6,40 +6,48 @@
 
 int hashSize;
 
-typedef struct _node{
+struct myNode{
 	char *name;
 	int marks;
 	struct node *next;
-}node;
+};
 
-typedef struct _hashtable{
-	node *first;
-}hash;
+struct myHashTable{
+	struct myNode *first;
+};
 
-hash** createHashTable(int hashTableSize){
+struct myNode* createNode(char *name, int marks){
+
+	struct myNode *newNode=(struct myNode*)malloc(sizeof(struct myNode));
 	
-	printf("hashtable size is %d\n",hashTableSize);
-	hash **hashTable = (hash **)malloc(hashTableSize * sizeof(hash*));
+	newNode->name=name;
+	newNode->marks=marks;
+	newNode->next=NULL;
 	
-	return hashTable;
+	return newNode;
 }
 
-hash* createNode(char *name, int marks){
+int insert(struct myHashTable hashTable[], char *name, int marks){
 
-	hash *node=(hash *)malloc(sizof(hash));
-	
-	
-	return node;
-}
-int insert(hash **hashTable, char *name, int marks){
-	
-	int hashBucket;
-	hash *node;
+	/* Calculating the bucketNumber, using the easiest hashFunction */
+	int hashBucket=marks%hashSize;
 
-	hashBucket=marks%hashSize;
-	node=createNode(name,marks);
+	printf("name is %s > marks is %d and hashBucket is %d\n",name,marks,hashBucket);	
 	
-	printf("name is %s > marks is %d and hashBucket is %d\n",name,marks,hashBucket);
+	/* Creating the node to be inserted */
+	struct myNode *newNode=createNode(name,marks),*temp;
+
+	if(!hashTable[hashBucket].first){
+		printf("Bucket is NULL, means this is the first element in the chaining.\n");
+		hashTable[hashBucket].first=newNode;	
+	}
+	else{
+		printf("Bucket is not Null, so inserting it to beginnning and updating the head value\n");
+		newNode->next=hashTable[hashBucket].first;	
+		
+	}
+
+	printf("name is %s > marks is %d and hashBucket is %d\n",hashTable[hashBucket].first->name,hashTable[hashBucket].first->marks,hashBucket);
 	
 	return 0;
 }
@@ -48,27 +56,50 @@ int main(int argc, char *argv[]){
 		
 	char *filename=argv[1], line[MAX], *tok, *name;
 	int foundHashSize=0, marks;
-	hash **hashTable;
-	
+
+	/* Just to calculate the size of HashTable */	
 	FILE *file=fopen(filename, "r");
 	while(fgets(line, sizeof line, file) != NULL){
+		
+		/* fetching the size of hashTable and breaking */
 		if(!foundHashSize){
-			hashSize=atoi(line);
-			
-			hashTable=createHashTable(hashSize);
-			
+                        hashSize=atoi(line);
+                        foundHashSize=1;
+                        break;
+                }
+	}
+	fclose(file);
+
+	/* Creating the hashTable with the userDefined Size */
+	foundHashSize=0;
+	struct myHashTable hashTable[hashSize];	
+	
+	/* Initializing the Bucket with NULL */
+	for(int index=0;index<hashSize;index++)
+		hashTable[index].first=NULL;
+
+	/* Insert operation of hashtable, is a mandatory operation */
+	FILE *fileRem=fopen(filename, "r");
+	while(fgets(line, sizeof line, file) != NULL){
+		
+		/* Skipping the very first line, sice this is just the size of hashTable */
+		if(!foundHashSize){
 			foundHashSize=1;
 			continue;
 		}
 		else{
+			/* tokenizing the line with colon (:) */
 			tok=strtok(line, ":");
 			name=tok;
 			tok=strtok(NULL, " ");
 			marks=atoi(tok);
 			tok=strtok(NULL, " ");
+
+			/* inserting the tokenized values i.e name and marks of the student */
 			insert(hashTable, name, marks);	
 		}
 	}
-	
+	fclose(fileRem);	
+
 	return 0;
 }
